2aa7b5931e50868e2fe9ca4b3854a4a183ccabe1
EdoardoDiLauro <s227649@studenti.polito.it>
2020-01-12 16:04:43 +0100

    form errors fixed

--- blog/main/routes.py
+++ blog/main/routes.py
@@ -7,8 +7,7 @@ main = Blueprint('main', __name__)
 @main.route("/")
 @main.route("/home")
 def home():
-    page = request.args.get('page', 1, type=int)
-    travels = Travel.query.order_by(Travel.date_posted.desc()).paginate(page=page, per_page=5)
+    travels = Travel.query.order_by(Travel.date_posted.desc())
     posts = Post.query.order_by(Post.date_posted.desc())
     return render_template('home.html', posts=posts, travels=travels)
 
